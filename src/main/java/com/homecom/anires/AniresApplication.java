af936aa9b7dbc26d1abbc814bf53f8d60a7595d9
Máté Nagy-Sándor <mate.nagy-sandor@lhsystems.com>
2025-06-16 22:22:19 +0200

    Initial commit

---
+++ src/main/java/com/homecom/anires/AniresApplication.java
@@ -0,0 +1,14 @@
+package com.homecom.anires;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AniresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AniresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/homecom/anires/AniresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.homecom.anires;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AniresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
